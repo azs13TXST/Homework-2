73d115944f306798b0051c069db94474fe5dae1d
Albert Soliz <azs13@txstate.edu>
2020-09-11 03:28:20 +0000

    nth root finder

--- nth_root_finder.cpp
+++ nth_root_finder.cpp
@@ -1,14 +1,32 @@
 /*
 PSEUDOCODE:
-
+FindNthRoot function
+  Initializes the int "temp"
+  Loops into teh set number
+    Loops to set temp
+      Returns the precision of the value
 
 */
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 string FindNthRoot(int number, int n, int precision) {
-    // TODO implement this properly
-    return "";
+  int temp;
+  
+  for (int j = 2; j <= number; j++)  {
+    temp = 1;
+    for (int k = 1; k <= n; k++)  {
+      temp *= j;
+      if (temp == number)  {
+        setprecision(precision) << i;
+        return i;
+      }
+    }
+  }
+  setprecision(precision) << temp;
+
+  return temp;
 }
